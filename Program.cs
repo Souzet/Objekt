d48656244f9ae7b00ed2a3f0c137faf643cdb7a0
souzet tadros <souzettadros@gmail.com>
2020-11-05 22:39:52 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objekt
+{
+    class Program
+    {
+        class Point
+        {
+            public double x, y;
+            public Point (double X, double Y)
+            {
+                x = X; y = Y;
+            }
+            public Point() { }
+        }
+        static void Main(string[] args)
+        {
+            Point p, q;
+            p = new Point() { x = 12.3, y = 17.5 };
+            q = new Point(12.3, 17.5);
+            Console.WriteLine($"p =<{p.x}, {p.y}>");
+            Console.WriteLine($"q =<{q.x}, {q.y}>");
+
+        }
+    }
+}
